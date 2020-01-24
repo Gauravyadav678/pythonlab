9b63958bcb8e371c947e1d65e9417242aa47cf4b
Gauravyadav678 <60250469+Gauravyadav678@users.noreply.github.com>
2020-01-24 19:45:42 +0530

    simple program for profit and loss

---
+++ profit.py
@@ -0,0 +1,8 @@
+a= input('what is the selling price')
+b=input('what is the cost price')
+c=a-b
+d=b-a
+if a>b:
+    print('there is profit of rupees',c,sep=' ')
+else:
+    print('there is loss of rupeees',d)
