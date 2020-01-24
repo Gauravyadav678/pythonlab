56a242caa2590a7d2558fb50b833fcd995ad298c
Gauravyadav678 <60250469+Gauravyadav678@users.noreply.github.com>
2020-01-24 20:06:35 +0530

    swaping of two numbers

---
+++ swaping.py
@@ -0,0 +1,5 @@
+a = int( input ( 'enter the value of a  ' ) )
+b = int( input ( 'enter the value of b ' ) )
+a , b = b , a
+print ( 'after the swaping a = ' , a )
+print ( 'after the swaping b = ' , b )
