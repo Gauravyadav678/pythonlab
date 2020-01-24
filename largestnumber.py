264c33475635cace563446775b48550575ae4728
Gauravyadav678 <60250469+Gauravyadav678@users.noreply.github.com>
2020-01-24 18:58:12 +0530

    find the largest number  among three numbers

---
+++ largestnumber.py
@@ -0,0 +1,10 @@
+a = int(input('enter the 1st number '))
+b = int(input('enter the  2nd '))
+c = int(input('enter the 3rd number '))
+if a >> b and a >> c :
+    print(' largest number is ' , a )
+elif b >> c :
+    print(' largest number is ' , b )
+else :
+    print(' largest number is ' , c )
+    
