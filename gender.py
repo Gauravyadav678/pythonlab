5f234038a6ac6f9f52427c17925c48e04828786a
Gauravyadav678 <60250469+Gauravyadav678@users.noreply.github.com>
2020-02-07 12:40:46 +0530

    enter your name and gender to check person is male or female

---
+++ gender.py
@@ -0,0 +1,7 @@
+a=input('enter your name')
+gender=input('enter your gender')
+if (gender=="male"or gender=="MALE"):
+    print(' your welcome mr.',a)
+elif (gender=="female"or gender=="FEMALE"):
+    print('your welcome mrs.',a)
+    
