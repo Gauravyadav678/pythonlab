c1d4284479d8aaffcc484690225b75daf765f096
Gauravyadav678 <60250469+Gauravyadav678@users.noreply.github.com>
2020-02-07 11:49:20 +0530

    how to print the repeat item in tuple

---
+++ repeat.py
@@ -0,0 +1,9 @@
+a=('prem','rani','ravi','rani','seeema','prem')
+b=[]
+for i in a:
+       if a.count(i)>1:
+                  if i not in b:
+                      b.append(i)
+print(b.clear)
+                        
+
